1fd4eda67bacad52bd531671ccc1fcd0f3121eb8
David Krystall <david.krystall@ontometrics.com>
2020-01-18 20:23:03 -0800

    removing more loops

--- euler/triples.py
+++ euler/triples.py
@@ -1,25 +1,18 @@
 #Question 9
+# pythagorean triple a<b<c, total is equal to a+b+c
 def findTriples(total):
-    searching = True
-    while searching:
-        for a in range(1,total):
-            if not searching:
-                break
-            if a == total-1:
-                print("Number not found")
-                searching = False
-            for b in range(a,total):
-                if not searching:
-                    break
-                c=total-(a+b)
-                if(c > b):
-                    aSquared, bSquared, cSquared  = a*a, b*b, c*c
-                    if (aSquared+bSquared == cSquared):
-                            if a+b+c == total:
-                                print(a,"+",b,"+",c,"=",total)
-                                print("Answer found!")
-                                print("Product of a,b,c is: ",a*b*c)
-                                searching = False
-                                if not searching:
-                                    break
+    for a in range(1,total):
+        if a == total-1:
+            print("Number not found")
+            searching = False
+        for b in range(a,total):
+            c=total-(a+b)
+            if(c > b):
+                aSquared, bSquared, cSquared  = a*a, b*b, c*c
+                if (aSquared+bSquared == cSquared):
+                        if a+b+c == total:
+                            print(a,"+",b,"+",c,"=",total)
+                            print("Answer found!")
+                            print("Product of a,b,c is: ",a*b*c)
+                            return a*b*c
 findTriples(1000)
